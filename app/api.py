aa41eb71b4c2b173f4d67685bb971b1d4010fbe4
liv-szt <oliviasztanga@gmail.com>
2021-01-21 16:17:16 -0500

    solution for day 1, part day 2

--- app/api.py
+++ app/api.py
@@ -2,3 +2,28 @@ from flask import request, render_template, redirect
 from app import app, db
 from models import User, Post
 
+@app.route('/', methods=['GET'])
+def get_index():
+    all_posts = Post.query.all()
+    serialized_all_posts = [item.serialize() for item in all_posts]
+    return render_template('index.html', all_posts=serialized_all_posts)
+
+@app.route('/user/<string:username>', methods=['GET'])
+def get_posts_by_user(username):
+    user = User.query.filter_by(username=username).first()
+    serialized_user = user.serialize()
+    return render_template('user.html', user=serialized_user)
+
+@app.route('/post/new', methods=['POST'])
+def submit_new_post():
+    username=request.form["username"]
+    content=request.form["content"]
+    user = User.query.filter_by(username).first()
+    if user == None:
+        user = User(username=username)
+        db.session.add(user)
+        db.session.commit()
+    post = Post(content=content, user_id=user.id)
+    db.session.add(post)
+    db.session.commit()
+    return redirect('/')
--- app/seed.py
+++ app/seed.py
@@ -3,11 +3,21 @@ import datetime
 
 def seed(db):
     users = [
-
+        User(id=1, username="bilbo"),
+        User(id=2, username="barbie"),
     ]
 
     posts = [
-
+        Post(
+            id=1,
+            content="Hello, my name is Bilbo. I am looking for a friend to accompany to Mordor.",
+            user_id=1,
+        ),
+        Post(
+            id=2,
+            content="Hello, I'm Barbie and I just found a really cool ring!!!",
+            user_id=2,
+        )
     ]
 
     db.session.add_all(users)
